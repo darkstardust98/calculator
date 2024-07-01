392eb1677637955a06ed2e73e66a7146c2cec86b
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-07-01 11:45:14 +0200

    ADDED event handlers to number buttons to set display_value

--- script.js
+++ script.js
@@ -57,13 +57,13 @@ function operate(num1, operate, num2) {
 // console.log(operate(20, "+", 2));
 // console.log(operate(20, "-", 2));
 
-let display_value = 0;
+let display_value = "";
 
 function update_display_value() {
     
 }
 
-//Number button event listeners
+//Number button event listener variable
 let button0 = document.getElementById("0");
 let button1 = document.getElementById("1");
 let button2 = document.getElementById("2");
@@ -75,10 +75,51 @@ let button7 = document.getElementById("7");
 let button8 = document.getElementById("8");
 let button9 = document.getElementById("9");
 
-//Operator button event listeners
+//Operator button event listener variable
 let button_add = document.getElementById("+");
 let button_subtract = document.getElementById("-");
 let button_multiply = document.getElementById("*");
 let button_divide = document.getElementById("/");
 let button_equals = document.getElementById("=");
 let button0_clear = document.getElementsByClassName("clear");
+
+//Number event listeners
+button0.addEventListener('click', function() {
+    display_value += "0";
+});
+
+button1.addEventListener('click', function() {
+    display_value += "1";
+});
+
+button2.addEventListener('click', function() {
+    display_value += "2";
+});
+
+button3.addEventListener('click', function() {
+    display_value += "3";
+});
+
+button4.addEventListener('click', function() {
+    display_value += "4";
+});
+
+button5.addEventListener('click', function() {
+    display_value += "5";
+});
+
+button6.addEventListener('click', function() {
+    display_value += "6";
+});
+
+button7.addEventListener('click', function() {
+    display_value += "7";
+});
+
+button8.addEventListener('click', function() {
+    display_value += "8";
+});
+
+button9.addEventListener('click', function() {
+    display_value += "9";
+});
