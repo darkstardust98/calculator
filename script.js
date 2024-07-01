4b6dcafdc7f5be88ba8ff538896ef0c3050d6e46
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-07-01 09:24:04 +0200

    ADDED first_number, operator and second_number vairables to script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function add(x,n) {
+function add(x, n) {
     accumulator = x;
     return accumulator += n;
 }
 
-function subtract(x,n) {
+function subtract(x, n) {
     accumulator = x;
     return accumulator -= n;
 }
+
+function multiply(x, n) {
+    accumulator = x;
+    return accumulator * n;
+}
+
+function divide(x, n) {
+    accumulator = x;
+    return accumulator / x;
+}
+
+let first_number = 0;
+let operator = 0;
+let second_number = 0;
+
